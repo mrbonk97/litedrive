38ad6235c43372dfa8852024e69d538b54492268
mrbonk97 <mrbonk97@gmail.com>
2025-10-28 01:33:20 +0900

    feat: 랜딩 페이지 내용 및 디자인 고도화

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ArrowUpRight } from "lucide-react";
 import { Logo } from "@/components/nav/logo";
-import { Pointer, Shield } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { Footer } from "@/components/nav/footer";
 
-export default async function Home() {
+async function Home() {
   return (
     <>
-      <main className="p-4 mx-auto max-w-7xl grid grid-cols-2 gap-4 gap-y-8">
+      <main className="">
         <h1 className="sr-only">LiteDrive</h1>
-        <header className="col-span-2 flex items-center justify-between gap-2">
+        <header className="p-4 mx-auto max-w-6xl flex items-center justify-between gap-2">
           <Logo />
           <nav className="text-xl font-medium flex gap-2">
             <Button variant={"secondary"} className="py-2" asChild>
@@ -21,37 +21,104 @@ export default async function Home() {
             </Button>
           </nav>
         </header>
-        <div className="col-span-2 md:col-span-1">
-          <p className="mt-4 mx-auto md:mx-0 text-2xl md:text-5xl max-w-md md:max-w-full font-bold leading-normal text-rose-400">
-            LiteDrive, 최소한의 보안으로 최대한의 속도. 파일만을 위한 가장 단순한 공유 플랫폼.
+        <section className="p-4 mt-8 sm:mt-8 mx-auto max-w-6xl grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <p className="mx-auto w-fit text-xl sm:text-4xl leading-normal font-bold sm:break-keep">
+            최소한의 보안으로 최대한의 속도.
+            <br /> 파일만을 위한 가장 단순한 공유 플랫폼.
           </p>
-          <Button className="mt-16 sm:mt-8 mx-auto md:mx-0 w-fit bg-rose-400" asChild>
-            <Link href={"/sign-up"}>시작하기</Link>
+          <Image
+            src={"/static/pattern.svg"}
+            alt="pattern"
+            height={1920}
+            width={1920}
+            quality={100}
+            className="p-8 sm:p-0 rounded"
+          />
+        </section>
+        <section className="mt-20 bg-secondary">
+          <div className="p-4 py-14 sm:py-28 mx-auto max-w-6xl">
+            <h4 className="text-4xl sm:text-5xl font-bold text-center">기능을 살펴보세요</h4>
+            <h5 className="mt-4 sm:mt-8 font-medium text-center opacity-70">몇개 없습니다...</h5>
+            <div>
+              <p className="mt-20 text-lg sm:text-3xl font-bold opacity-80">1. 대시보드</p>
+              <Image
+                src={"/static/feature/litedrive-3.png"}
+                alt="feature-dashboard"
+                height={940}
+                width={1660}
+                quality={100}
+                className="mt-2 sm:mt-4 shadow-2xl rounded"
+              />
+            </div>
+            <div className="mt-40">
+              <p className="mt-20 text-lg sm:text-3xl font-bold opacity-80">2. 파일공유</p>
+              <Image
+                src={"/static/feature/litedrive-5.png"}
+                alt="feature-share-1"
+                height={940}
+                width={1660}
+                quality={100}
+                className="mt-2 sm:mt-4 shadow-2xl rounded"
+              />
+              <Image
+                src={"/static/feature/litedrive-6.png"}
+                alt="feature-share-2"
+                height={940}
+                width={1660}
+                quality={100}
+                className="mt-8 shadow-2xl rounded"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="p-4 py-14 sm:py-28 mx-auto max-w-6xl">
+          <h2 className="text-2xl sm:text-4xl font-bold leading-relaxed">
+            어차피 무료입니다. <br />
+            무엇보다 <span className="text-rose-400">안전</span>하고요. <br />
+            그러니까, 사용해보세요.
+          </h2>
+
+          <ol className="mt-20 space-y-16">
+            <li className="p-4 bg-secondary rounded text-4xl font-bold">
+              <strong className="text-rose-400">1.</strong> 보안
+              <p className="p-4 text-sm sm:text-lg font-semibold break-keep">
+                보안 전문가가 직접 설계하고 취약점 검사를 수행한 시스템입니다. LiteDrive는 안전한
+                전송을 최우선으로 생각합니다.
+              </p>
+            </li>
+
+            <li className="p-4 bg-secondary rounded text-4xl font-bold">
+              <strong className="text-rose-400">2.</strong> 완전한 삭제
+              <p className="p-4 text-sm sm:text-lg font-semibold break-keep">
+                파일 삭제나 회원 탈퇴 시{" "}
+                <span className="text-rose-400">soft delete는 없습니다.</span>
+                모든 데이터는 즉시 완전 삭제되어,
+                <span className="text-rose-400">복구가 불가능하도록 설계</span>되었습니다.
+              </p>
+            </li>
+
+            <li className="p-4 bg-secondary rounded text-4xl font-bold">
+              <strong className="text-rose-400">3.</strong> 편의성
+              <p className="p-4 text-sm sm:text-lg font-semibold break-keep">
+                단순히 파일만 빠르게 주고받고 싶은데, 매번 2차 인증이 번거로우셨나요? 공용 PC에서
+                개인 계정 로그인이 불안하셨다면, LiteDrive는 가볍게 회원가입 후 바로 사용할 수
+                있습니다.
+              </p>
+            </li>
+          </ol>
+        </section>
+        <section className="p-4 mb-60 mx-auto max-w-6xl">
+          <Button asChild>
+            <Link href={"/sign-up"} className="ml-auto block w-fit bg-rose-400">
+              <span>시작하기</span>
+              <ArrowUpRight />
+            </Link>
           </Button>
-        </div>
-        <Image
-          src={"/static/pattern.svg"}
-          alt="bg"
-          height={1000}
-          width={1000}
-          className="hidden md:block h-full w-full object-cover opacity-80"
-        />
-        <div className="mt-8 col-span-1 p-4 aspect-video border rounded flex flex-col justify-between">
-          <h4 className="text-2xl font-bold opacity-80">보안</h4>
-          <Shield className="my-24 mx-auto text-rose-400" size={96} />
-          <p className="text-center text-sm opacity-80 break-keep">
-            탈퇴 시 모든 데이터는 즉시 파기되며, 복구가 불가능합니다.
-          </p>
-        </div>
-        <div className="mt-8 col-span-1 p-4 aspect-video border rounded flex flex-col justify-between">
-          <h4 className="text-2xl font-bold opacity-80">편의성</h4>
-          <Pointer className="my-24 mx-auto text-rose-400" size={96} />
-          <p className="text-center text-sm opacity-80 break-keep">
-            2FA 등 부가 기능 없이, 가장 가볍고 빠른 공유에 집중합니다.
-          </p>
-        </div>
+        </section>
       </main>
       <Footer />
     </>
   );
 }
+
+export default Home;
--- components/nav/footer.tsx
+++ components/nav/footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./logo";
 export function Footer() {
   return (
     <footer className="mt-20 p-4 bg-secondary">
-      <section className="pt-8 mx-auto max-w-7xl h-96">
+      <section className="pt-8 mx-auto max-w-6xl h-96">
         <Logo />
         <div className="mt-4 text-sm font-semibold opacity-80">문의: hyunsuk1997@naver.com</div>
       </section>
